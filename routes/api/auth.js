f677ee6570946c94ca64929d3a66bb81fcb9f856
Gabrieliusv <gabrielius.svi@gmail.com>
2019-08-21 21:44:37 +0300

    Changed routes

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -12,59 +12,84 @@ const User = require('../../models/User');
 //@desc Test route
 //@access Public
 
-router.get('/', auth, async (req, res) => {
-  try {
-    const user = await User.findById(req.user.id).select('-password');
-    res.json(user);
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send('Server Error');
+router.get(
+  '/',
+  [
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Password is required').exists()
+  ],
+  auth,
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select('-password');
+      res.json(user);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
   }
-});
+);
 
 // @route Post api/auth
 //@desc Authenticate user & get token
 //@access Public
 
-router.post('/', async (req, res) => {
-  const { email, password } = req.body;
-
-  try {
-    let user = await User.findOne({ email });
-
-    if (!user) {
-      return res
-        .status(400)
-        .json({ errors: { msg: 'Email or password is incorrect' } });
+router.post(
+  '/',
+  [
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Password is required').exists()
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
 
-    const isMatch = await bcrypt.compare(password, user.password);
+    const { email, password } = req.body;
 
-    if (!isMatch) {
-      return res
-        .status(400)
-        .json({ errors: { msg: 'Email or password is incorrect' } });
+    try {
+      let user = await User.findOne({ email });
+
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: { msg: 'Email or password is incorrect' } });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ errors: { msg: 'Email or password is incorrect' } });
+      }
+
+      const payload = {
+        user: {
+          id: user._id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
     }
-
-    const payload = {
-      user: {
-        id: user._id
-      }
-    };
-
-    jwt.sign(
-      payload,
-      config.get('jwtSecret'),
-      { expiresIn: 360000 },
-      (err, token) => {
-        if (err) throw err;
-        res.json({ token });
-      }
-    );
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send('Server error');
   }
-});
+);
 
 module.exports = router;
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
-const { check, validationResult } = require('express-validator');
 
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
@@ -17,7 +16,7 @@ router.get('/me', auth, async (req, res) => {
     );
 
     if (!profile) {
-      return res.status(400).json({ msg: 'There is no profile for this user' });
+      return res.status(404).json({ msg: 'There is no profile for this user' });
     }
 
     res.json(profile);
@@ -30,60 +29,45 @@ router.get('/me', auth, async (req, res) => {
 //@route POST api/profile
 //@desc Create or update user profile
 //@access Private
-router.post(
-  '/',
-  [
-    auth,
-    [
-      check('bio', 'Bio ir required')
-        .not()
-        .isEmpty()
-    ]
-  ],
-  async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+router.post('/', async (req, res) => {
+  const { location, bio, youtube, twitter, facebook, instagram } = req.body;
+
+  // Build Profile object
+  const profileFields = {};
+  profileFields.user = req.user.id;
+  if (bio) profileFields.bio = bio;
+  if (location) profileFields.location = location;
+
+  profileFields.social = {};
+  if (youtube) profileFields.social.youtube = youtube;
+  if (twitter) profileFields.social.twitter = twitter;
+  if (facebook) profileFields.social.facebook = facebook;
+  if (instagram) profileFields.social.instagram = instagram;
+
+  try {
+    let profile = await Profile.findOne({ user: req.user.id });
+
+    //Update profile
+    if (profile) {
+      profile = await Profile.findOneAndUpdate(
+        { user: req.user.id },
+        { $set: profileFields },
+        { new: true }
+      );
+
+      return res.json(profile);
     }
-    const { location, bio, youtube, twitter, facebook, instagram } = req.body;
 
-    // Build Profile object
-    const profileFields = {};
-    profileFields.user = req.user.id;
-    if (bio) profileFields.bio = bio;
-    if (location) profileFields.location = location;
+    //Create Profile
+    profile = new Profile(profileFields);
 
-    profileFields.social = {};
-    if (youtube) profileFields.social.youtube = youtube;
-    if (twitter) profileFields.social.twitter = twitter;
-    if (facebook) profileFields.social.facebook = facebook;
-    if (instagram) profileFields.social.instagram = instagram;
-
-    try {
-      let profile = await Profile.findOne({ user: req.user.id });
-
-      //Update profile
-      if (profile) {
-        profile = await Profile.findOneAndUpdate(
-          { user: req.user.id },
-          { $set: profileFields },
-          { new: true }
-        );
-
-        return res.json(profile);
-      }
-
-      //Create Profile
-      profile = new Profile(profileFields);
-
-      await profile.save();
-      res.json(profile);
-    } catch (err) {
-      console.error(err.message);
-      res.sratus(500).send('server Error');
-    }
+    await profile.save();
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.sratus(500).send('server Error');
   }
-);
+});
 
 //@route DELETE api/profile
 //@desc Delete current users profile
